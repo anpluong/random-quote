dcab648a1a437d33007e11701a6f3c7336c4b999
anpluong <thinkingtea@gmail.com>
2018-06-30 01:41:38 -0700

    complete the random quote

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     super(props);
     this.state = {
       quote: '', 
-      author: ''
+      author: '',
+      colors: ['orange', 'blue', 'green', 'yellow', 'purple', 'gray', 'red', 'silver'],
+      number: 0
     };
 
     this.getRandomQuote();
@@ -23,8 +25,9 @@ class App extends Component {
         .then(response => {          
           this.setState({
             quote: response.data.quote,
-            author: response.data.author
-          });
+            author: response.data.author,
+            number: Math.floor(Math.random() * this.state.colors.length)
+          });          
       });
   }
 
@@ -33,8 +36,13 @@ class App extends Component {
   }
 
   render() {    
+   
+   let style = {
+     backgroundColor: this.state.colors[this.state.number]
+   }
+   
    return (
-     <div class='container'>
+     <div className='container' style={style}>
       <div id='quote-box'>
         <div id='text'>        
             {this.state.quote}
@@ -42,11 +50,12 @@ class App extends Component {
         <div id='author'>
             {this.state.author}
         </div>        
-        <div>
-            <button type='button' onClick={() => this.onClickEvent()}> Get a random Quote</button>
-        </div>
+        <div id='btn-block'>
+            <button id='btn-text' type='button' onClick={() => this.onClickEvent()}>New Quote</button>
+        </div>  
       </div>
       </div>
+
     )
   }
 }
